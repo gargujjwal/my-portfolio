daa674a579dcef7f38bc8495ef0d4f8b4ee739fa
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2023-10-02 20:24:27 +0530

    add header section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,32 @@
 import "./globals.css";
 
-import { Inter } from "next/font/google";
+import Header from "@/components/Header";
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Ujjwal | Personal Portfolio",
-    description:
-        "Ujjwal is a full-stack web and app developer with 2 years of experience.",
+  title: "Ujjwal | Personal Portfolio",
+  description:
+    "Ujjwal is a full-stack web and app developer with 2 years of experience.",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en">
-            <body
-                className={`${inter.className} relative bg-gray-50 text-gray-950`}
-            >
-                <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
-                <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
-
-                {children}
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} relative h-[5000px] bg-gray-50 text-gray-950`}
+      >
+        <div className="absolute right-[11rem] top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full bg-[#fbe2e3] blur-[10rem] dark:bg-[#946263] sm:w-[68.75rem]"></div>
+        <div className="absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-[#dbd7fb] blur-[10rem] dark:bg-[#676394] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
 }
