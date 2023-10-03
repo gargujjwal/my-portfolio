fee7ae9f054430cf222f0e6451e70a278762ce93
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2023-10-03 12:53:03 +0530

    add intro section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,26 +7,27 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Ujjwal | Personal Portfolio",
-  description:
-    "Ujjwal is a full-stack web and app developer with 2 years of experience.",
+    title: "Ujjwal | Personal Portfolio",
+    description:
+        "Ujjwal is a full-stack web and app developer with 2 years of experience.",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body
-        className={`${inter.className} relative h-[5000px] bg-gray-50 text-gray-950`}
-      >
-        <div className="absolute right-[11rem] top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full bg-[#fbe2e3] blur-[10rem] dark:bg-[#946263] sm:w-[68.75rem]"></div>
-        <div className="absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-[#dbd7fb] blur-[10rem] dark:bg-[#676394] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
-        <Header />
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body
+                className={`${inter.className} relative h-[5000px] bg-gray-50 pt-28 text-gray-950`}
+            >
+                <div className="absolute right-[11rem] top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full bg-[#fbe2e3] blur-[10rem] dark:bg-[#946263] sm:w-[68.75rem]"></div>
+                <div className="absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-[#dbd7fb] blur-[10rem] dark:bg-[#676394] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+                <Header />
+
+                {children}
+            </body>
+        </html>
+    );
 }
